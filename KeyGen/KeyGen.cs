72d75e2116634f784cf0d979e88bd42cdf5bbbdd
andrey <andrey.kozhevnikov@devexpress.com>
2024-04-07 14:21:24 +0400

    get private and wif

--- KeyGen/KeyGen.cs
+++ KeyGen/KeyGen.cs
@@ -1,7 +1,43 @@
-﻿namespace KeyGenNameSpace {
+﻿using SimpleBase;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace KeyGenNameSpace {
     public class KeyGen {
         public static AddressSet GenerateFromString(string value) {
-            return new AddressSet();
+            byte[] bytes = Encoding.ASCII.GetBytes(value);
+            SHA256 mySHA256 = SHA256.Create();
+            byte[] hashValue = mySHA256.ComputeHash(bytes);
+
+            var st = BitConverter.ToString(hashValue);
+
+            return GenerateFromBytes(hashValue);
+        }
+        public static AddressSet GenerateFromBytes(byte[] bytes) {
+            //   byte[] bytes = Encoding.ASCII.GetBytes(value);
+            var hexPrivate = Convert.ToHexString(bytes).ToLower();
+            var fullKey = "80" + hexPrivate + "01";
+            SHA256 mySHA256 = SHA256.Create();
+            byte[] fullKeybytes = Convert.FromHexString(fullKey);
+            var sha1 = mySHA256.ComputeHash(fullKeybytes);
+            //var sha1Hex = Convert.ToHexString(sha1);
+            //byte[] sha1Hexbytes = Convert.FromHexString(sha1Hex);
+            var sha2 = mySHA256.ComputeHash(sha1);
+            //   var sha3= mySHA256.ComputeHash(sha1);
+            // var st2 = Convert.ToHexString(sha2);
+            var sha2hex = Convert.ToHexString(sha2);
+
+            var checkSum = sha2hex.Substring(0,8);
+            var wifString = fullKey + checkSum;
+            var wifBytes=Convert.FromHexString(wifString);
+            string wif = Base58.Bitcoin.Encode(wifBytes);
+
+
+            var adr = new AddressSet();
+            adr.PrivateKey = hexPrivate;
+            adr.WIF = wif;
+            return adr;
         }
     }
 }
--- TestsKeyGenerator/KeyGeneratorTests.cs
+++ TestsKeyGenerator/KeyGeneratorTests.cs
@@ -19,6 +19,30 @@ namespace TestsKeyGenerator {
 
 
         }
+        [Test]
+        public void Test1adr() {
+            var input = "enter credit long demand tortoise harsh frame path rifle news then trigger";
+            var res = KeyGen.GenerateFromString(input);
+
+            var hasAdr = res.Addresses.Contains("1CUuQXtKLY4XoEbHZ9BexWLZfN7wUHPuXC");
+            Assert.AreEqual(true, hasAdr);
+        }
+        [Test]
+        public void Test3adr() {
+            var input = "enter credit long demand tortoise harsh frame path rifle news then trigger";
+            var res = KeyGen.GenerateFromString(input);
+
+            var hasAdr = res.Addresses.Contains("3KJq5p2MtPyV4i6yJRboxNkXCob197MiJj");
+            Assert.AreEqual(true, hasAdr);
+        }
+        [Test]
+        public void TestBCadr() {
+            var input = "enter credit long demand tortoise harsh frame path rifle news then trigger";
+            var res = KeyGen.GenerateFromString(input);
+
+            var hasAdr = res.Addresses.Contains("bc1q0hc9c4jh39nz2tf8plsyfjsajthg4x4uza7u40");
+            Assert.AreEqual(true, hasAdr);
+        }
         //[Test]
         //public void Test2() {
         //    var input = "11F53BE6DC18F1DD5DC9F111D83EB93F0F9B09BDA1812B000000000000000000";
